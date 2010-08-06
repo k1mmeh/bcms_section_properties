3246bb1f0f9c892ad77b99c96d8f8eaf3426a04e
Kimmy Chirnside <kimmy.chirnside@gmail.com>
2010-08-07 11:57:05 +1200

    Testing fix - migrating to other test environment

--- db/migrate/20100713095728_create_section_properties.rb
+++ db/migrate/20100713095728_create_section_properties.rb
@@ -3,6 +3,7 @@ class CreateSectionProperties < ActiveRecord::Migration
     create_table :section_properties do |t|
       t.column :name, :string, :limit => 128
       t.column :key, :string, :limit => 128
+      t.column :property_type, :string, :limit => 16  # text, string, boolean, numeric
       t.timestamps
     end
 
@@ -12,6 +13,8 @@ class CreateSectionProperties < ActiveRecord::Migration
       t.column :value, :text
       t.timestamps
     end
+
+    SectionProperty.create(:name => 'Analytics Code', :key => 'analytics-code', :property_type => 'text')
   end
 
   def self.down
--- lib/bcms_analytics_tools/models/section.rb
+++ lib/bcms_analytics_tools/models/section.rb
@@ -1,21 +1,74 @@
 Section # must ensure that the core section class loads first
-class Section < ActiveRecord::Base
 
-  has_many :section_property_values
+Section.has_many :section_property_values
+#Section.after_save :set_new_section_property_values
+#module SectionPropertyExtensions
+#  module ClassMethods
 
-  def section_property_by_key(key)
-    property = SectionProperty.find_by_key(key)
-    return nil unless property
-    
-    @section_property_values ||= self.section_property_values
-    return @section_property_values.detect {|spv| spv.section_property_id = property.id}
-  end
+#  end
 
-  def show_section_property(key)
-    property = section_property_by_key(key)
-    return nil unless property
+#  module InstanceMethods
+#      end
+#end
 
-    return property.value
-  end
+#Section.send :include, SectionPropertyExtensions::InstanceMethods
+
+class Section
+#  has_many :section_property_values
+#  require 'ruby-debug'
+#  after_save :set_new_section_property_values
+
+  def section_property_from_key(key)
+      property = SectionProperty.find_by_key(key)
+      return nil unless property
+
+      return self.section_property_values.detect {|p| p.section_property_id == property.id}
+    end
+
+    def show_section_property(key)
+      property = section_property_from_key(key)
+      return nil unless property
+
+      return property.value
+    end
+
+    def section_properties=(properties_hash)
+      return unless properties_hash.is_a?(Hash)
+
+      update, add = [], []
+      properties_hash.each do |k, v|
+        sp = SectionProperty.find_by_key(k)
+        next unless sp
+
+        #if existing = self.section_property_from_key(k)
+        #  existing.value = v
+        #  update << existing
+        #else
+          add << {:section_property_id => sp.id, :value => v}
+        #end
+      end
+
+      self.section_property_values.build(add)
+#      @new_section_property_values = update + add
+    end
+
+    def section_properties
+      #return @new_section_property_values if @new_section_property_values
+
+      return self.section_property_values
+    end
+
+    private
+
+      def set_new_section_property_values
+        logger.error(@new_section_property_values.class)
+        
+        if @new_section_property_values
+          self.section_property_values = @new_section_property_values
+        end
+
+        return true
+      end
+
 
 end
